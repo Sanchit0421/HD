e861ca6229dcfd1af4971445bf2e9c081e90e358
Sanchit0421 <aggarwalsanchit2005@gmail.com>
2024-10-06 14:14:23 +1100

    Create App.js

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+
+function App() {
+    return (
+        <div>
+            <h1>Chameleon Smart Energy Dashboard</h1>
+            <Dashboard />
+        </div>
+    );
+}
+
+export default App;

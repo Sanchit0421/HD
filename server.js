24b8b4c6918ade0a1f432d7674b8add8d2850f4f
Sanchit0421 <aggarwalsanchit2005@gmail.com>
2024-10-06 14:50:31 +1100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
-const energyRoutes = require('./energy');  // Import energy.js directly
+const energyRoutes = require('./energy'); // Import energy.js directly
 
 dotenv.config();
 
@@ -10,6 +10,11 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON
 app.use(express.json());
+
+// Root route
+app.get('/', (req, res) => {
+    res.send('Welcome to the Chameleon Smart Energy Dashboard API!');
+});
 
 // Use energy routes
 app.use('/api/energy', energyRoutes);
@@ -26,3 +31,4 @@ mongoose.connect(process.env.MONGO_URI, {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+

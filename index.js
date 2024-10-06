ef272cae748a7088de0ff28e033319b98b02de79
Sanchit0421 <aggarwalsanchit2005@gmail.com>
2024-10-06 20:17:57 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App'; // Make sure this points to the correct file
+import './index.css'; // If you have a CSS file
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
